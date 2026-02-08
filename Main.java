c742a3a5c40f72e5b2be253b2d8e321c75678062
eduardaFloripo <eduardafloripoxavier@gmail.com>
2026-02-08 15:58:50 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import aplicacao.ACMEAirDrones;
+
+public class Main {
+    public static void main(String[] args) {
+        ACMEAirDrones a1 = new ACMEAirDrones();
+        a1.executar();
+    }
+}
